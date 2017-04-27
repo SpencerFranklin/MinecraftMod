a1fa8634559f2dc4d10061ac7f593a4e2876a614
David Fasano <dfaz3@ccs.neu.edu>
2017-04-27 19:46:42 -0400

    lower that case

--- src/main/java/net/spencer/test/Reference.java
+++ src/main/java/net/spencer/test/Reference.java
@@ -39,7 +39,7 @@ public class Reference {
 public static enum ModBlocks {
 		
 		CHEESE("cheese", "cheese_block"),
-		MPB("MPB", "MPB"),
+		MPB("mpb", "mbp"),
 		SPREAD("spread", "spread");
 
 		private String unlocalizedName;
