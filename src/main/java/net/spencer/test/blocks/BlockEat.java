8503e2d8d739e6be93db5ef126432b6ed76ada3b
David Fasano <dfaz3@ccs.neu.edu>
2017-04-27 23:19:44 -0400

    Attempt #1

--- src/main/java/net/spencer/test/blocks/BlockEat.java
+++ src/main/java/net/spencer/test/blocks/BlockEat.java
@@ -8,6 +8,7 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockAir;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentString;
@@ -28,44 +29,39 @@ public class BlockEat extends Block {
 
 	@Override
 	public void randomTick(World worldIn, BlockPos pos, IBlockState state, Random random) {
-		double d = Math.random();
-		BlockPos p = pos;
-		Random r = new Random();
-		int res = r.nextInt(5 - 1) + 1;
+		BlockPos p = pos.down();
+		if(!worldIn.getBlockState(p).getBlock().isAir(worldIn.getBlockState(p), worldIn, p)) {
+			worldIn.setBlockState(p, ModBlocks.eat.getDefaultState());
+			worldIn.scheduleUpdate(p, worldIn.getBlockState(p).getBlock(), 0);
+		}
+		p = pos.up();
+		if(!worldIn.getBlockState(p).getBlock().isAir(worldIn.getBlockState(p), worldIn, p)) {
+			worldIn.setBlockState(p, ModBlocks.eat.getDefaultState());
+			worldIn.scheduleUpdate(p, worldIn.getBlockState(p).getBlock(), 0);
+		}
+		p = pos.east();
+		if(!worldIn.getBlockState(p).getBlock().isAir(worldIn.getBlockState(p), worldIn, p)) {
+			worldIn.setBlockState(p, ModBlocks.eat.getDefaultState());
+			worldIn.scheduleUpdate(p, worldIn.getBlockState(p).getBlock(), 0);
+		}
+		p = pos.west();
+		if(!worldIn.getBlockState(p).getBlock().isAir(worldIn.getBlockState(p), worldIn, p)) {
+			worldIn.setBlockState(p, ModBlocks.eat.getDefaultState());
+			worldIn.scheduleUpdate(p, worldIn.getBlockState(p).getBlock(), 0);
+		}
+		p = pos.south();
+		if(!worldIn.getBlockState(p).getBlock().isAir(worldIn.getBlockState(p), worldIn, p)) {
+			worldIn.setBlockState(p, ModBlocks.eat.getDefaultState());
+			worldIn.scheduleUpdate(p, worldIn.getBlockState(p).getBlock(), 0);
+		}
+		p = pos.north();
+		if(!worldIn.getBlockState(p).getBlock().isAir(worldIn.getBlockState(p), worldIn, p)) {
+			worldIn.setBlockState(p, ModBlocks.eat.getDefaultState());
+			worldIn.scheduleUpdate(p, worldIn.getBlockState(p).getBlock(), 0);
+		}
 		
-		switch (res) {
-			case 1:	 p = pos.east();
-
-			case 2:	 p = pos.west();
-			
-			case 3:	 p = pos.north();			
-
-			case 4:	 p = pos.south();
-		}
-		 
-		if (d < 1) {
-			if (worldIn.getBlockState(p).getBlock() != ModBlocks.eat) {
-				worldIn.setBlockState(p, ModBlocks.eat.getDefaultState(), 3);
-				worldIn.scheduleUpdate(p, worldIn.getBlockState(p).getBlock(), 0);
-			}
-		}
-		
-		if (d < .5) {
-			if (d < .25){
-				if (worldIn.getBlockState(pos.down()).getBlock() != ModBlocks.eat) {
-					worldIn.setBlockState(pos.down(), ModBlocks.eat.getDefaultState(), 3);
-					worldIn.scheduleUpdate(pos.down(), worldIn.getBlockState(pos.down()).getBlock(), 0);
-				}
-			} else {
-				if (worldIn.getBlockState(pos.up()).getBlock() != ModBlocks.eat) {
-					worldIn.setBlockState(pos.up(), ModBlocks.eat.getDefaultState(), 3);
-					worldIn.scheduleUpdate(pos.up(), worldIn.getBlockState(pos.up()).getBlock(), 0);
-				}
-			}
-			
-		}
-
 		this.updateTick(worldIn, pos, state, random);
+		worldIn.setBlockState(pos, Blocks.AIR.getDefaultState());
 	}
 
 }
